efaa58b7bb57e4e126e787daa51aa08f6c94e58d
LeandroF01 <leaylio3327@gmail.com>
2023-03-31 15:32:12 -0300

    Refacator whit migration sequelize to models db shoppingcart

---
+++ src/utils/migrations/20230329231158-add-shopping-cart-id-to-purchases.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		/**
+		 * Add altering commands here.
+		 *
+		 * Example:
+		 * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+		 */
+		await queryInterface.addColumn("purchases", "shoppingcart_id", {
+			type: Sequelize.UUID,
+			allowNull: false,
+			references: {
+				model: "shoppingCarts",
+				key: "id",
+			},
+		});
+	},
+
+	async down(queryInterface, Sequelize) {
+		/**
+		 * Add reverting commands here.
+		 *
+		 * Example:
+		 * await queryInterface.dropTable('users');
+		 */
+		await queryInterface.removeColumn("purchases", "shoppingcart_id");
+	},
+};
