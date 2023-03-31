e7a81cfae90483fdc39d5e496e6f75735ed39842
LeandroF01 <leaylio3327@gmail.com>
2023-03-31 15:30:10 -0300

    Add controllers, services and router of  auth users

---
+++ src/auth/services/auth.services.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken");
+const { jwtSecret } = require("../../config");
+const { loginUser } = require("../controllers/auth.controllers");
+
+const login = (req, res) => {
+	const { email, password } = req.body;
+
+	if (email && password) {
+		loginUser(email, password)
+			.then((response) => {
+				if (response) {
+					const token = jwt.sign(
+						{
+							id: response.id,
+							email: response.email,
+							role: response.role,
+						},
+						jwtSecret
+					);
+					res.status(200).json({
+						message: "Correct Credentials",
+						token,
+					});
+					console.log(token);
+				} else {
+					res.status(401).json({ message: "Invalid Credentials" });
+				}
+			})
+			.catch((error) => {
+				res.status(400).json({ message: error.message });
+			});
+	} else {
+		res.status(400).json({ message: "Missing Data" });
+	}
+};
+
+module.exports = {
+	login,
+};
